28da6ef7ebd0f93fcadd335569baa2637dd0b93c
Rahul Mohandas <rahul0705@gmail.com>
2026-09-01 11:23:52 -0400

    feat(template): add gated Pages deployment (#109)

--- templates/default/src/pages/rss.xml.ts
+++ templates/default/src/pages/rss.xml.ts
@@ -2,6 +2,7 @@ import rss from '@astrojs/rss';
 
 import { site } from '../config/site';
 import { getArticles } from '../lib/articles';
+import { resolveSiteHref } from '../lib/paths';
 
 export const GET = async () =>
   rss({
@@ -11,7 +12,7 @@ export const GET = async () =>
     items: (await getArticles()).map((article) => ({
       title: article.data.title,
       description: article.data.description,
-      link: `/articles/${article.id}/`,
+      link: resolveSiteHref(`/articles/${article.id}/`),
       pubDate: article.data.publishedAt,
     })),
   });
